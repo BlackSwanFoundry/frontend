a7ee8754224cc794dccbaae53576847576642625
Cypherious Buildtonius <cypher.build@protonmail.com>
2022-05-09 23:53:21 -0500

    Finished BasicToken
    basic token definition is essentially complete.
    still need to write transfer / approval tests to ensure functionality.

--- migrations/2_basic_token.js
+++ migrations/2_basic_token.js
@@ -1,5 +1,5 @@
 const BasicToken = artifacts.require("BasicToken");
-const Constants = require("consts")
+const Constants = require( "../consts.js");
 
 module.exports = function (deployer) {
   deployer.deploy(BasicToken, Constants.basicTokenName, Constants.basicTokenSymbol, Constants.basicTokenDecimals, Constants.basicTokenSupply);
--- test/basic_token.js
+++ test/basic_token.js
@@ -1,15 +1,44 @@
 const BasicToken = artifacts.require("BasicToken");
-const Constants = require("consts");
+const Constants = require( "../consts.js");
+const BN = require("bn.js");
 
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("BasicToken", function (/* accounts */) {
+contract("BasicToken", function (accounts) {
+  const contractOwner = accounts[0];
+  const alice = accounts[1];
+  const bob = accounts[2];
+
   it("Correct Name", async function () {
     const instance = await BasicToken.deployed();
     const name = await instance.name.call();
     return assert.isTrue(name === Constants.basicTokenName, "Basic Token Name is incorrect.");
   });
+
+  it("Correct Symbol", async function () {
+    const instance = await BasicToken.deployed();
+    const symbol = await instance.symbol.call();
+    return assert.isTrue(symbol === Constants.basicTokenSymbol, "Basic Token Symbol is incorrect.");
+  });
+
+  it("Correct Decimals", async function () {
+    const instance = await BasicToken.deployed();
+    const decimals = await instance.decimals.call();
+    return assert.isTrue(decimals.toNumber() === Constants.basicTokenDecimals, "Basic Token Decimals is incorrect.");
+  });
+
+  it("Correct Initial Supply", async function () {
+    const instance = await BasicToken.deployed();
+    const supply = await instance.totalSupply.call();
+    return assert.isTrue(supply.toNumber() === Constants.basicTokenSupply, "Basic Token Initial Supply is incorrect.");
+  });
+
+  it("Correct Initial Token Ownership", async function(){
+    const instance = await BasicToken.deployed();
+    const ownerBalance = await instance.balanceOf.call(contractOwner);
+    return assert.isTrue(ownerBalance.toNumber() == Constants.basicTokenSupply, "Basic Token Initial Ownership is incorrect.");
+  })
 });
